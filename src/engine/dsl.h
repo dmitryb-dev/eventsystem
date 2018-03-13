9aad8cc0209944d3c4d1b178a4f082bbb269ec71
Dmitry Barannik <uratyd@gmail.com>
2018-03-13 12:58:03 +0200

    dsl refactoring

--- src/engine/dsl.h
+++ src/engine/dsl.h
@@ -19,12 +19,12 @@
 
 int _evs_ok;
 int _evs_fail;
-#define publishData(EventName, varDef) \
-    for (varDef = _evs_state##EventName = _evs_create##EventName(); \
-         (_evs_state##EventName != &_evs_ok && _evs_state##EventName != &_evs_fail) || \
-         (_evs_state##EventName == &_evs_ok && _evs_commit##EventName()); \
-         _evs_state##EventName = _evs_state##EventName? &_evs_ok : &_evs_fail \
-    ) if (_evs_state##EventName)
+#define publishData(Event, varDef) \
+    for (varDef = _evs_state##Event = _evs_create##Event(); \
+         (_evs_state##Event != &_evs_ok && _evs_state##Event != &_evs_fail) ||  /* Not processed yet */ \
+         (_evs_state##Event == &_evs_ok && _evs_commit##Event());               /* Commit after processing */ \
+         _evs_state##Event = _evs_state##Event? &_evs_ok : &_evs_fail \
+    ) if (_evs_state##Event)
 
 #define handleEvent(Name) _evs_handle##Name();
 
