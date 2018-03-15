bda498811bde59d96f58c440fd16a4f0467058fc
Dmitry Barannik <uratyd@gmail.com>
2018-03-15 14:23:49 +0200

    System lifecycle define corrected

--- src/engine/system/system.h
+++ src/engine/system/system.h
@@ -1,15 +1,17 @@
 #ifndef RUN_SYSTEM_ENGINE_H
 #define RUN_SYSTEM_ENGINE_H
 
+#ifdef SYSTEM_LIFECYCLE
+    int _evs_handleSystemStart();
+    int _evs_handleSystemTick();
+    int _evs_handleSystemStop();
+#endif
+
+void _evs_systemStep();
+
 int _evs_isEventSystemStopped;
 int _evs_isSystemStart;
 int _evs_systemStepId;
-
-int _evs_handleSystemStart();
-int _evs_handleSystemTick();
-int _evs_handleSystemStop();
-
-void _evs_systemStep();
 
 void runEventSystem()
 {
